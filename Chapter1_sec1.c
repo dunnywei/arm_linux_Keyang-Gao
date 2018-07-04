eeaa7142f00a8e575f4a903d74c8afb86d12332c
dunnywei <wei_don@yahoo.com>
2018-07-03 22:38:51 -0400

    Chapter1_sec1 (4:57)

---
+++ Chapter1_sec1.c
@@ -0,0 +1,15 @@
+/*
+-https://www.youtube.com/watch?v=pet3bOhOXzY&index=1&list=PLhqasd25bl-hamUHgiK7ooKBjt0y7L5XS
+Unit 1->ARM register, ARM CPU mode, ARM assembly, 
+
+Unit 2->External hardware and device driver
+
+Clock->Memory, Flash, port, data sheet, manual
+
+Unit 3->ARM boot loader
+
+Like BIOS
+Boot->like the embedded board to intialized by the minimum hardware enviroment (4:57)
+loader->
+  
+*/
