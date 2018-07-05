32bc952a26b2250672cb784b857d78e8750d7338
dunnywei <wei_don@yahoo.com>
2018-07-04 20:49:29 -0400

    Chapter1_sec1.c (@8:42)

--- Chapter1_sec1.c
+++ Chapter1_sec1.c
@@ -15,7 +15,19 @@ Like BIOS
 -Boot->like the embedded board to intialized by the minimum hardware enviroment (4:57)
 -loader->Let the OS(Linux/Android) inside the flash/hard drive (5:17) into the RAM (5:44)
 and redy to execute (5:49)
--U_boot->open source (6:14). They are using tiny-210 development board (6:49)
+-U_boot->open source (6:14). They are using tiny-210 development board (6:49) based on ARM 
+cortex A8 (1GHz) (@7:48)
+
+Unit 4->ARM interrupt programming (@8:02)
+-Software interrupt
+-Hardware interrupt(@8:14)
+-U must know the interrupt bofore going to the OS. Otherwise, it won't help(@8:26)
+-When there is a context switch(切换) required for swapping out one process running on the CPU. It requires
+interrupt(@8:42)
+
+
+
+
 
 
 
