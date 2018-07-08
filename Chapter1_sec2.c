71ed88d70a9e74638c3263908d9081fad262eac1
dunnywei <wei_don@yahoo.com>
2018-07-08 18:20:30 -0400

    (1:27)

--- Chapter1_sec2.c
+++ Chapter1_sec2.c
@@ -5,7 +5,14 @@ https://www.youtube.com/watch?v=GddifYkmZsI&index=2&list=PLhqasd25bl-hamUHgiK7oo
 -Coretex corresponding to instruction set architeture v7 (0:05)
 -If the code developed for ARMv7, it will work for ARM Cortex M. But not vice versa (0:21)
 -ARM CPU architecuter (0:43)
--If you want to know arm assembly (汇编)(0:48) or programming, u have to understand how the register(寄存器) works
+-If you want to know arm assembly (汇编)(0:48) or programming in arm, u have to understand how the register(寄存器) works
 and cooperate. (0:53)
+->In x86, what kind of registers do we have?(1:03)
+  -EAX, EBX,ECX,EDX (1:14)
+  -ESP, EIP
+  -EFLAG (1:18)
+->In ARM (1:23), there are 37 registers in side ARM (1:27)
+
+
 
 */
