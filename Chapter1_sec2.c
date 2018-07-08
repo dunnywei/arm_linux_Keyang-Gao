e2602da19c32ad25008b00caca9ac7772494ce1a
dunnywei <wei_don@yahoo.com>
2018-07-08 16:08:17 -0400

    (0:53)

--- Chapter1_sec2.c
+++ Chapter1_sec2.c
@@ -3,5 +3,9 @@ link->
 https://www.youtube.com/watch?v=GddifYkmZsI&index=2&list=PLhqasd25bl-hamUHgiK7ooKBjt0y7L5XS
 
 -Coretex corresponding to instruction set architeture v7 (0:05)
+-If the code developed for ARMv7, it will work for ARM Cortex M. But not vice versa (0:21)
+-ARM CPU architecuter (0:43)
+-If you want to know arm assembly (汇编)(0:48) or programming, u have to understand how the register(寄存器) works
+and cooperate. (0:53)
 
 */
