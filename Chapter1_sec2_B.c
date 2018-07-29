7465c18416c6d09b9d6a4305429510e2548d165a
dunnywei <wei_don@yahoo.com>
2018-07-28 22:47:35 -0400

    (30:31)

--- Chapter1_sec2_B.c
+++ Chapter1_sec2_B.c
@@ -1,5 +1,8 @@
 //After (30:00)
 //https://www.youtube.com/watch?v=GddifYkmZsI&index=2&list=PLhqasd25bl-hamUHgiK7ooKBjt0y7L5XS
-->We have RAIN3 as Non Priviledge (非特权级别) with lowest priority and 
-          RAIN0 as privilege      (特权级别)   with highest priority
-->(30:09)
+->RAIN3 as Non Priviledge (非特权级别) with lowest priority->(ARM) User
+->RAIN0 as priviledge(特权级别) with highest priority-> (ARM) FIQ (快速中断), IRQ 普通中断
+                                                            , Supervisor超级管理模式)(监视模式)
+                                                            , Abort (终止), Undefined(未定义),System (系统)
+(30:09)(30:20)(30:31)
+->
