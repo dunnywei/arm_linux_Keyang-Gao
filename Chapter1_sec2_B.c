76f5d07a952b9851e66aa89daaf98e749880e6c2
dunnywei <wei_don@yahoo.com>
2018-07-28 22:33:47 -0400

    (30:09)

---
+++ Chapter1_sec2_B.c
@@ -0,0 +1,5 @@
+//After (30:00)
+//https://www.youtube.com/watch?v=GddifYkmZsI&index=2&list=PLhqasd25bl-hamUHgiK7ooKBjt0y7L5XS
+->We have RAIN3 as Non Priviledge (非特权级别) with lowest priority and 
+          RAIN0 as privilege      (特权级别)   with highest priority
+->(30:09)
