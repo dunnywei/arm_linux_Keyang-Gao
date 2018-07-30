e83c6ec477bedcb52f0b257f78c2862561557f7c
dunnywei <wei_don@yahoo.com>
2018-07-29 21:32:38 -0400

    (31:27)

--- Chapter1_sec2_B.c
+++ Chapter1_sec2_B.c
@@ -3,6 +3,7 @@
 ->RAIN3 as Non Priviledge (非特权级别) with lowest priority->(ARM) User
 ->RAIN0 as priviledge(特权级别) with highest priority-> (ARM) FIQ (快速中断), IRQ 普通中断
                                                             , Supervisor超级管理模式)(监视模式)
-                                                            , Abort (终止), Undefined(未定义),System (系统)
-(30:09)(30:20)(30:31)
-->
+                                                            , Abort (终止模式), Undefined(未定义指令模式),System (系统模式)
+(30:09)(30:20)(30:31)(31:16)
+->IF CPU is at priviledge level, it is like running at RAIN0 (31:24), it can access to any address
+in the kernel space in the memory (内核内存) (31:27)
