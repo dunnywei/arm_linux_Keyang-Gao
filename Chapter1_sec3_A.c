c4bcd27e9520be26c706d6449a0eef45adc778cf
dunnywei <wei_don@yahoo.com>
2018-08-05 20:20:44 -0400

    (2:27)

--- Chapter1_sec3_A.c
+++ Chapter1_sec3_A.c
@@ -1,3 +1,5 @@
+->3 ARM汇编指令构造格式
+
 //https://www.youtube.com/watch?v=Xi3ioi1213k&list=PLhqasd25bl-hamUHgiK7ooKBjt0y7L5XS&index=3
 ->(0:00)
 ->We have to be familiar with ARM registers especailly ,r13 (SP), r14 (LR) and CPSR (0:21)
@@ -15,7 +17,9 @@
 ->Pipeline 流水线(1:33)
   -Number of instruction set the CPU can handle (1:46). It doens't means that the instructions sets are executing at the same time (1:52)
   -It is at different stages (阶段) (1:54). 
-  -For example (1:58)
+  -For example (1:58), (2:15) When CPU want to execute an instructions set (指令), it has
+  to fetch the instruction from memory (内存) (2:22)
+  -the instruction set in 4 bytes 字节 is stored into cache (快速缓冲贮存区) or 预取器 of the CPU (2:27)
 ->
 ->
 ->
