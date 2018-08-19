a1b38f256d27b77f1c0096ee98f5b61327542e51
dunnywei <wei_don@yahoo.com>
2018-08-18 20:53:08 -0400

    (37:09)

--- Chapter1_sec3_B.c
+++ Chapter1_sec3_B.c
@@ -13,12 +13,13 @@ https://www.youtube.com/watch?v=Xi3ioi1213k&list=PLhqasd25bl-hamUHgiK7ooKBjt0y7L
     -
 ->Difference between Cond条件码 and s码 (35:52)
     -The working time (工作时间点) is different (36:00)
-    -One is done before (前) the insturction set (指令) of branch being executed (36:02)
+    -One (Cond条件码) is done before (前) the insturction set (指令) of branch being executed (36:02)
       -b is for branch (跳转) and eq is 条件码 (Condition code) (36:50)
       -before branching, I need to see if condition (条件) of beq is met (成立) or not(36:55)
-      -Once the Cond条件码 is met (成立), (37:00) I will do the branch (跳转) (36:59)
-      -Cond条件码 applies (37:09)
-    -The other is done after(后) the intruction set (指令) of branch being executed (37:07)
+      -Once the Cond条件码 is met (成立), (37:00) I will do the branch (跳转) (36:59). Otherwise I won't jump (37:03)
+      -The determination of Cond条件码 is done BEFORE executing the instruction set (指令) of  branch (37:09)
+    -The other (s码) is done after(后) the intruction set (指令) of branch being executed (37:07).
+        
     -
 -> 
 ->
