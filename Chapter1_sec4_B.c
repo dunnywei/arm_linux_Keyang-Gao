7fb96a011b6ca7cbcd2a49f75d038b7666493030
dunnywei <wei_don@yahoo.com>
2018-09-07 22:30:30 -0400

    (40:51)

--- Chapter1_sec4_B.c
+++ Chapter1_sec4_B.c
@@ -3,7 +3,27 @@ Linux嵌入式开发和驱动开发--1ARM体系结构和汇
 link->https://www.youtube.com/watch?v=4qziMvvMCjo&index=4&list=PLhqasd25bl-hamUHgiK7ooKBjt0y7L5XS
 
 ->(39:51)
-->
+->ORR (Or 或 bitwise operator) (40:05)
+->EOR (异或EOR) (40:14)
+  -link->https://blog.csdn.net/decisiveness/article/details/46312607
+  Exlusive OR->1 EOR 1=0, 0 EOR 0=0,1 EOR 0=1 (40:27)
+  -eg
+AREA asm1test,CODE,READONLY
+   ENTRY
+AAA
+   MOV R0, #0X1
+   MOV R1, R0
+   MOV R2, #0X2
+   
+   ADD R3,R1,R2  ;R3=R1+R2 (26:54)
+   SUB R4,R3,R1  ;R4=R3-R1 (26:35)分号 (26:43) 
+   SUBS R0,R0,R0
+   
+   MVN R5,#0X0 ;
+   AND R3,R1,R2; (40:51)
+
+   B .
+   END
 ->
 ->
 ->
