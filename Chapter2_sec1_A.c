e3b4319fbde7baa859487bcf722580a27cf46462
dunnywei <wei_don@yahoo.com>
2018-09-30 20:48:20 -0400

    (16:07)

--- Chapter2_sec1_A.c
+++ Chapter2_sec1_A.c
@@ -8,7 +8,7 @@ Section1_c和汇编混合编程
 ->Use C lanquage to call the function (调用函数) written in assembly or vice versa (0:28)(0:58)
 ->How to passing the paramter (传递参数) between C and assemlby during function call 函数调用(0:58)
 ->In C programming lanquage, we have two parameters (参数) (1:06)
-    -In C lanquage
+    -Code 1
 int fun(int a,int b)
 {
     
@@ -19,8 +19,53 @@ int main(void)
 }
     -They are using stack (堆栈)(2:56) to pass the parameter (参数) from RIGHT to LEFT (3:36)
     -So it will starts from 6 then to 4 a.k.a 压栈 (3:55)
-->
-->
+    -graphical view
+
+
+
+3G---------------------------------
+   6
+   4
+        
+   main
+   fun
+
+
+
+    -main and fun are both saved in the code segment (代码段) (4:28)
+    -(starts here)
+        
+       
+->code 2 (10:05)
+void bubble_sort(int a[],int n);
+
+/*bubble.s*/
+bubble_sort
+   move r0
+   
+
+/*main.c*/
+int main()
+{
+    int a[10];
+    bubble_sort(a,10);
+}
+->code 3 (15:10)(16:07)
+#include <stdio.h>
+
+extern int myadd(int a,int b);
+
+int main(void)
+{
+   int a=3;
+   int b=5;
+   int c;
+   c=myadd(a,b);
+   printf("%d\n",c);
+   
+   return 0;
+}
+    
 ->
 ->
 ->
