5bf372d981d1ea8688f54d858c467bd22374f399
dunnywei <wei_don@yahoo.com>
2019-08-11 19:35:19 -0400

    (20:05)

--- Chapter2_sec2_A.c
+++ Chapter2_sec2_A.c
@@ -144,6 +144,16 @@ Section2_计算机存储介质
         -
         -     
 ->Memory map->super important (19:57)
+        -all of the address showing up here is physical address 物理地址 (20:05)
+        -
+        -
+        -
+        -
+        -
+        -
+        -
+        -
+        -
 ->
 ->
 ->
