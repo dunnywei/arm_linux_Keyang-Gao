3b6bbe083a3ecfb38fafa8fa6526b4764200359c
dunnywei <wei_don@yahoo.com>
2019-09-07 20:24:46 -0400

    (23:17)

--- Chapter2_sec2_A.c
+++ Chapter2_sec2_A.c
@@ -174,12 +174,13 @@ Section2_计算机存储介质
         -for v210,we have 512MB and refers to DRAM_0 
         -Yeserday we have the "Load a value then go 0x2160 0000" so we use the memory address at DRAM_0 (22:17)
         -For DRAM_1,it be applied for expansion however it is not connected yet(22:36)
+        -It is used to save stack, heap,global inside phyical memory (23:17)
         
  ->For SFRS(special function register特殊功能寄存器) from 0xE000_0000 to 0xFFFF_FFF (22:46)
-        -It is used to save the special register          
+        -It is used to save the all register          
         -it refers to "control register" 控制寄存器 and "data register" 数据寄存器. This is what we have done yesterday in Chapter1_sec5_A.c (22:58) 
-        -
-        -
+        -We used the physial address of "0xE020 8000" (23:09)
+        -(23:17)
         -
         -
         -
