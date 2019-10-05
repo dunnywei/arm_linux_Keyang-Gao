6aed31d26fc7ee8f573ba5d8f6a3d90b0ab323cc
dunnywei <wei_don@yahoo.com>
2019-10-05 19:56:13 -0400

    adding pyramid

--- Chapter2_sec2_A.c
+++ Chapter2_sec2_A.c
@@ -256,9 +256,21 @@ https://blog.csdn.net/politefish/article/details/5594741
         -
         -
         -
-        -          
-                
+        -  
 ->
+->Register 寄存器,  CPU triggerer  (1 ns)
+->Cache缓存, SRAM, 随机存取固态硬碟(random access solid state drive) (10ns), SRAM,DDRAM,
+电容表显示( capacitor gauge display) telling u when you should recharge your battery,refresh刷新
+->Hard drive 硬盘
+        -Nor flash   ,Intel , small in storage size, 2m to 32m, save bios/bootloader 
+        -NAND flash,Toshibal, large in storage size,1g to 256G,it acts as if it is a hard drive 硬盘          
+        -
+        -
+        -
+        -
+        -
+        -
+        -
 ->
 ->
 ->
