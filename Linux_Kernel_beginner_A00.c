abeed5c65974cebfc34bc8c5bf7d8a5137d362f7
dunnywei <wei_don@yahoo.com>
2020-01-22 14:07:50 -0500

    (08:43)

--- Linux_Kernel_beginner_A00.c
+++ Linux_Kernel_beginner_A00.c
@@ -110,8 +110,8 @@ arm-none-linux-gnueabi-gcc -v
         -In development, we will utilize "/tftpboot" folder  (07:56)
                  -Under the "tftpboot" folder, we will have uimage and dtb file (08:04)
                  -The rootfs file system will be under "/opt/rootfs" (08:11)
-                 -
-                 -
+                 -The reason is to mimic the same file system in DDR for the final product (8:27)
+                 -U-BOOT will load the "uimage" and "dtf file system" from tcp/ip or udp to the developmeent board->like load VxWorks.stc to the development board (8:43)
                  -
                  -
          
