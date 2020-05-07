0d81446bd27b2a1ca2872956c93ae8e1ca292632
wei_don@yahoo.com <wei_don@yahoo.com>
2020-05-06 23:50:23 -0400

    adding another info

--- TCP_UDP_connection.c
+++ TCP_UDP_connection.c
@@ -1,7 +1,9 @@
-link1->By java in UDP
+link01->By java in UDP
 https://stackoverflow.com/questions/9520911/java-sending-and-receiving-file-byte-over-sockets
-link1->
-link1->
+link02->datagram-ftp example in java
+github.com/naz-mul/datagram-ftp
+
+link03->
 link1->
 link1->
 link1->
