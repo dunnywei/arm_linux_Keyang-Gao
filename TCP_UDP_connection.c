442712b2b5fb16c5d54394aa15634ba02941c9ee
wei_don@yahoo.com <wei_don@yahoo.com>
2020-05-10 18:45:43 -0400

    change

--- TCP_UDP_connection.c
+++ TCP_UDP_connection.c
@@ -3,8 +3,10 @@ https://stackoverflow.com/questions/9520911/java-sending-and-receiving-file-byte
 link02->datagram-ftp example in java
 github.com/naz-mul/datagram-ftp
 
-link03->
-link1->
+link03->TFTP protocol
+https://www.youtube.com/results?search_query=%E5%B0%81%E8%A3%85+tftp
+link04->tftp beginner
+https://www.emertxe.com/trainings/technical-resource-library/embedded-systems/linux-internals-network-programming-course-contents/linux-internals-network-programming-industry-projects-list/project3trivial-file-transfer-protocol-tftp/
 link1->
 link1->
 link1->
