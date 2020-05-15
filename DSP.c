83ba01fdcb74846b4806c8c8ad7f118b11c3240a
dunnywei <wei_don@yahoo.com>
2020-05-15 15:06:48 -0400

    update

--- DSP.c
+++ DSP.c
@@ -4,3 +4,8 @@ https://www.youtube.com/watch?v=mkGsMWi_j4Q
 2->discrete fourier transform vs continuous
 https://medium.com/sho-jp/tagged/digital-signal-processing
 http://www.robots.ox.ac.uk/~sjrob/Teaching/SP/l7.pdf
+
+3->DSP all
+https://www.youtube.com/watch?v=jnxTpxB7HR8
+
+
