7af062f10fe0a2cf5df769d1b20ae0c9722362ad
dunnywei <wei_don@yahoo.com>
2020-06-12 10:56:53 -0400

    update

--- work.c
+++ work.c
@@ -2,3 +2,7 @@
 https://github.com/GeonTech/XMidasBlueReader
 2->Plantium blue python
 https://github.com/bdeeg08/framework-core/blob/master/src/base/framework/python/ossie/utils/bluefile/bluefile_helpers.py
+
+->Midas BLUE python
+//search for 6000
+https://github.com/RedhawkSDR/framework-core/blob/master/src/base/framework/python/ossie/utils/bluefile/bluefile.py
