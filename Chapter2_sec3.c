e44122de78ca1e6a6a84a2e72e0334d703d3ffce
dunnywei <wei_don@yahoo.com>
2021-11-07 20:03:24 -0500

    adding comment for GPIO

---
+++ Chapter2_sec3.c
@@ -0,0 +1,4 @@
+->Chapter 2_3->0:59
+	-why there are so many "S5PV210"? since there 
+	are so more than 500 pins so each one module will relates to each functionality. Each "S5V210" relates to CAM (3:19)
+	-
