f16970c9e587f50cde0cd92e7adbd3849afce542
dunnywei <wei_don@yahoo.com>
2021-11-07 23:47:16 -0500

    continue 1

--- Chapter2_sec3.c
+++ Chapter2_sec3.c
@@ -1,4 +1,10 @@
 ->Chapter 2_3->0:59
 	-why there are so many "S5PV210"? since there 
 	are so more than 500 pins so each one module will relates to each functionality. Each "S5V210" relates to CAM (3:19)
-	-
+	-4 Memory chips (4:36) as K4T1G84QQ
+		-1G means 1GByte=1024MB (4:53)
+			-according to->https://blog.csdn.net/wangjun5159/article/details/49126875
+		-There are 8 units
+		-1GByte/8=128 MB (5:22)
+		-Each unit has 128 MByte (一兆字节) (5:34)
+		-Four chips will make up 512 MByte (兆) (5:43)
