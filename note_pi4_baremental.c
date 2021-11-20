34dfbdd324e695d74a3be9684f9fc63cd8ca1438
dunnywei <wei_don@yahoo.com>
2021-11-19 23:11:49 -0500

    adding pi4 related

---
+++ note_pi4_baremental.c
@@ -0,0 +1,10 @@
+Tutorial 1->https://www.youtube.com/results?search_query=rasperry+pi+4+bare+metal
+	-#define PBASE	0xFE000000
+	-(12:39)
+	-we can see from the datasheet as " visible to the ARM at 0x0_FEnn_nnnn if Low Peripheral mode is enabled"
+	-we can check the rasperry pi device tree as bcm2711.dtsi (16:29)
+	->we will see (16:29)
+range=<0x7e0000 0x0 0xfe0000
+
+Tutorial 2->https://www.youtube.com/watch?v=r3Ye08ktcMo
+
